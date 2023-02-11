ce4ef8e70f9863eb5c398a145dab66a0689290b2
OLAPTEV <o.laptev@xceptance.net>
2023-02-11 21:47:44 +0100

    Added main class

---
+++ src/main/java/com/wavtat/chatboot/SpringChatApp.java
@@ -0,0 +1,16 @@
+package com.wavtat.chatboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                SpringChatApp.class , args);
+    }
+
+}
